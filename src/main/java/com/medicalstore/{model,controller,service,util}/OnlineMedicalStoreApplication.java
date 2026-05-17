f0297e8d8cb5c7e71f74c43addf43449fe1ade15
IT25101459 <IT25101459@my.sliit.lk>
2026-05-18 01:27:30 +0530

    Add files via upload

---
+++ src/main/java/com/medicalstore/{model,controller,service,util}/OnlineMedicalStoreApplication.java
@@ -0,0 +1,11 @@
+package com.medicalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMedicalStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMedicalStoreApplication.class, args);
+    }
+}
